684cd227f7d7333319c8cc9088cc5a5af0ec14f8
Shamya Karumbaiah <shamya16@gmail.com>
2018-01-28 20:54:13 -0500

    Update calculate_L.py

--- calculate_L.py
+++ calculate_L.py
@@ -218,7 +218,7 @@ stud_seq.append(stud)
 aff_seq.append([obs1, obs2])  
 print "number of students", len(aff_seq)
 print "number of coders", len(aff_seq[1])
-print "number of observations per student (assumes same number for all)", len(aff_seq[60][1])
+print "number of observations per student (assumes same number for all)", len(aff_seq[0][1])
 dMello_L_stats = call_dMello_L(aff_seq, stud_seq, self_trans = SELF_TRANS)
 write_to_csv(dMello_L_stats)
 print "COMPLETE."
